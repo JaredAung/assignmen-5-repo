6f6a31aba85ddfec31157d218b88c71974b9501e
JaredAung <133064427+JaredAung@users.noreply.github.com>
2024-03-16 20:25:11 -0700

    Update main.js

--- resources/main.js
+++ resources/main.js
@@ -2,7 +2,7 @@ function AddNumbers(){
     var number1 = parseFloat(document.getElementById("num1").value);
     var number2 = parseFloat(document.getElementById("num2").value);
     var result = number1  + number2;
-    document.getElementById('result').innerText = "The sum is "+ result;
+    document.getElementById("result").innerText = "The sum is "+ result;
 
 }
 var addButton = document.getElementById("Addition");
