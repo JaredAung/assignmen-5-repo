cfb228d0a3fa58a3b6d6a4d9f9c1f5584fdc4256
student <student@WebDev2023.myguest.virtualbox.org>
2024-03-16 20:23:28 -0700

    Updated

--- resources/main.js
+++ resources/main.js
@@ -1,9 +1,9 @@
 function AddNumbers(){
-    var number1 = parseFloat(document.getElementById(num1).value);
-    var number2 = parseFloat(document.getElementById(num2).value);
+    var number1 = parseFloat(document.getElementById("num1").value);
+    var number2 = parseFloat(document.getElementById("num2").value);
     var result = number1  + number2;
-    document.getElementById('result').innerText = 'The sum is '+ result;
+    document.getElementById('result').innerText = "The sum is "+ result;
 
 }
-var addButton = document.getElementById('Addition');
+var addButton = document.getElementById("Addition");
 addButton.addEventListener("click", AddNumbers);
