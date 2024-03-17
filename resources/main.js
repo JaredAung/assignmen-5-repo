edf208c4da092fa718e656c3643626f6fc62ea45
student <student@WebDev2023.myguest.virtualbox.org>
2024-03-16 20:09:24 -0700

    fixing button

--- resources/main.js
+++ resources/main.js
@@ -1,8 +1,8 @@
 function AddNumbers(){
-    var number1 = parseFloat(document.getElementByID(num1).value);
+    var number1 = parseFloat(document.getElementById(num1).value);
     var number2 = parseFloat(document.getElementById(num2).value);
     var result = number1  + number2;
-    document.getElementById(result).value = 'The sum is '+ result;
+    document.getElementById(result).innerText = 'The sum is '+ result;
 
 }
 var addButton = document.getElementById('Addition');
